955a1808d827806b05121022c81fe51ca7366786
eliang <eliang.cs@gmail.com>
2013-12-15 22:58:02 +0800

    Proof of concept: Be able to convert a big5 file to utf8.

---
+++ background.js
@@ -0,0 +1,21 @@
+/* global chrome */
+
+'use strict';
+
+chrome.app.runtime.onLaunched.addListener(function() {
+    // Center window on screen.
+    var screenWidth = screen.availWidth;
+    var screenHeight = screen.availHeight;
+    var width = 500;
+    var height = 300;
+
+    chrome.app.window.create('index.html', {
+        id: 'convertxWinID',
+        bounds: {
+            width: width,
+            height: height,
+            left: Math.round((screenWidth-width)/2),
+            top: Math.round((screenHeight-height)/2)
+        }
+    });
+});
--- main.js
+++ main.js
@@ -1,21 +1,68 @@
-/* global chrome */
+var iconv = require('iconv-lite');
+console.log(iconv);
 
-'use strict';
+console.log('big5: ' + iconv.encodingExists('big5'));
+console.log('ascii: ' + iconv.encodingExists('ascii'));
+console.log('utf-8: ' + iconv.encodingExists('utf-8'));
 
-chrome.app.runtime.onLaunched.addListener(function() {
-    // Center window on screen.
-    var screenWidth = screen.availWidth;
-    var screenHeight = screen.availHeight;
-    var width = 500;
-    var height = 300;
+function waitForIO(writer, callback) {
+  // set a watchdog to avoid eventual locking:
+  var start = Date.now();
+  // wait for a few seconds
+  var reentrant = function() {
+    if (writer.readyState===writer.WRITING && Date.now()-start<4000) {
+      setTimeout(reentrant, 100);
+      return;
+    }
+    if (writer.readyState===writer.WRITING) {
+      console.error("Write operation taking too long, aborting!"+
+        " (current writer readyState is "+writer.readyState+")");
+      writer.abort();
+    }
+    else {
+      callback();
+    }
+  };
+  setTimeout(reentrant, 100);
+}
 
-    chrome.app.window.create('index.html', {
-        id: 'convertxWinID',
-        bounds: {
-            width: width,
-            height: height,
-            left: Math.round((screenWidth-width)/2),
-            top: Math.round((screenHeight-height)/2)
-        }
+$(function() {
+
+    $('#btn-choose').click(function() {
+        console.log('clicked');
+        chrome.fileSystem.chooseEntry({ type: 'saveFile' }, function(entry, fileEntries) {
+            console.log(entry);
+            console.log(fileEntries);
+
+            entry.file(function(file) {
+                var reader = new FileReader();
+                reader.onerror = function(e) {
+                    console.log(e);
+                };
+                reader.onload = function(e) {
+                    var str = e.target.result;
+                    var text = iconv.decode(str, 'big5');
+
+                    console.log(text);
+
+                    buf = iconv.encode(text, 'utf8');
+
+                    var blob = new Blob([buf], { type: 'text/plain' });
+                    entry.createWriter(function(writer) {
+                        writer.truncate(blob.size);
+                        waitForIO(writer, function() {
+                            writer.seek(0);
+                            writer.write(blob);
+                        });
+                    });
+
+                    /*chrome.fileSystem.getWritableEntry(entry, buf, function(e) {
+                        console.log('Write complete :)');
+                    });*/
+                };
+                reader.readAsBinaryString(file);
+            });
+        });
     });
+
 });
